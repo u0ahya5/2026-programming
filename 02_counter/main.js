2279aca60cc3a8d8d568454aab2d90e6e75079b1
u0ahya5 <s2429@e-mirim.hs.kr>
2026-03-23 14:14:46 +0900

    counter JS onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -6,12 +6,17 @@ const plusButton = document.getElementsByTagName("button")[0];
 
 // button 누르면 숫자 증가
 // function 지우고 화살표로도 가능
-plusButton.addEventListener("click", () => {
-    count++; // count += 1; // count = count + 1;
-    resultH1.innerHTML = count;
-}); // 만약 처리함수()라고 적어둔다면 클릭할 때 처리함수를 실행할 리턴값(undefind)이 들어가서
+// plusButton.addEventListener("click", () => {
+//     count++; // count += 1; // count = count + 1;
+//     resultH1.innerHTML = count;
+// }); // 만약 처리함수()라고 적어둔다면 클릭할 때 처리함수를 실행할 리턴값(undefind)이 들어가서
                                             // 숫자가 증가를 못한다 언디파인드가 실행된다
                                             // 어떤 함수를 실행 할지만 적혀 있어야 한다
+
+plusButton.onclick = () => {
+    count++; // count += 1; // count = count + 1;
+    resultH1.innerHTML = count;
+};
 
 // 증가한 숫자 화면에 표시하자
 // function 처리함수(){
