4dfc90afa8dca2213dde2f6128944a6eed711d38
u0ahya5 <s2429@e-mirim.hs.kr>
2026-04-06 14:34:46 +0900

    TodoListApp HTML

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,5 +1,27 @@
 function TodoListApp(){
-
+    return(
+        <div>
+            <h1>ToDo List</h1>
+            <form>
+                <input type="textbox" placeholder="할 일을 입력하세요."/>
+                <button>Add</button>
+            </form>
+            <ul>
+                <li>
+                    <input type="checkbox" id="chk-1"></input>
+                    <label htmlFor="chk-1">놀기</label>
+                    <button>✏️</button>
+                    <button>❌</button>
+                </li>
+                <li>
+                    <input type="checkbox"></input>
+                    <label htmlFor="">집가기</label>
+                    <button>✏️</button>
+                    <button>❌</button>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default TodoListApp;
